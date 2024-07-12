76c69f499b354ebaec63ad801729e8e35b397d1a
ryan.jennings <ryan.jennings@sit.ac.nz>
2024-07-12 21:11:12 +1200

    End of section 6

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -27,7 +27,20 @@ public static class Config
                 RedirectUris = { "http://www.getpostman.com/whatever/oauth2/callback" },
                 ClientSecrets = new[] {new Secret("NotASecret".Sha256())},
                 AllowedGrantTypes = {GrantType.ResourceOwnerPassword}
+            },
+            new Client
+            {
+                ClientId = "nextApp",
+                ClientName = "nextApp",
+                ClientSecrets = {new Secret("secret".Sha256)},
+                AllowedGrantTypes = GrantTypes.CodeAndCLientCredentials,
+                RequirePkce = false,
+                RedirectUris = {"http://localhost:3000/api/auth/callback/id-server"},
+                AllowOfflineAccess = true,
+                AllowedScopes = {"openid", "profile", "auctionApp"},
+                AccessTokenLifetime = 3600*24*30
             }
+
             // m2m client credentials flow client
             // new Client
             // {
